c6539dde127b58429f7a0e6e1b62700b85067694
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2016-10-02 17:08:37 +0530

    // https://codility.com/programmers/task/odd_occurrences_in_array/ // Lesson 2: Arrays // OddOccurrencesInArray // Find value that occurs in odd number of elements.
    // A non-empty zero-indexed array A consisting of N integers
    // is given. The array contains an odd number of elements,
    // and each element of the array can be paired with another
    // element that has the same value, except for one element
    // that is left unpaired.
    
    // For example, in array A such that:
    //   A[0] = 9  A[1] = 3  A[2] = 9
    //   A[3] = 3  A[4] = 9  A[5] = 7
    //   A[6] = 9
    
    //         the elements at indexes 0 and 2 have value 9,
    //         the elements at indexes 1 and 3 have value 3,
    //         the elements at indexes 4 and 6 have value 9,
    //         the element at index 5 has value 7 and is unpaired.
    
    // Write a function:
    
    //     int solution(int A[], int N);
    
    // that, given an array A consisting of N integers fulfilling
    // the above conditions, returns the value of the unpaired element.
    
    // For example, given array A such that:
    //   A[0] = 9  A[1] = 3  A[2] = 9
    //   A[3] = 3  A[4] = 9  A[5] = 7
    //   A[6] = 9
    
    // the function should return 7, as explained in the example above.
    
    // I recall that this is the same as the HackerRank Lonely Integer
    // problem:
    // https://github.com/Sathyaish/HackerRank/tree/master/LonelyInteger

---
+++ L2.Arrays.OddOccurrencesInArray/L2.Arrays.OddOccurrencesInArray/Extensions.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace L2.Arrays.OddOccurrencesInArray
+{
+    public static class Extensions
+    {
+        public static void Print<T>(this IEnumerable<T> source,
+            TextWriter writer = null,
+            string separator = ", ",
+            string leadingMessage = null,
+            bool newLineAfterSequence = true,
+            bool throwOnEmptySource = true,
+            string emptySequenceMessage = "No items to print.",
+            string bracket = "[]")
+        {
+            TextWriter w = writer ?? Console.Out;
+            IEnumerable<string> mutated = null;
+            var count = 0;
+            var builder = new StringBuilder();
+            string openingBracket = null;
+            string closingBracket = null;
+
+            if (source == null || source.Count() == 0)
+            {
+                if (throwOnEmptySource) throw new ArgumentNullException("source");
+
+                emptySequenceMessage.Print(w, newLineAfterSequence);
+                return;
+            }
+
+            if (bracket != null && bracket.Length != 2)
+            {
+                throw new ArgumentException("Invalid bracket");
+            }
+
+            openingBracket = bracket[0].ToString();
+            closingBracket = bracket[1].ToString();
+
+            count = source.Count();
+            if (count == 1)
+            {
+                builder.AppendFormat($"{leadingMessage}{openingBracket}{source.First()?.ToString()}{closingBracket}");
+                builder.ToString().Print(w, newLineAfterSequence);
+                return;
+            }
+
+            mutated = source.Take(count - 1)
+                .Select(item => string.Format($"{item.ToString()}{separator}"))
+                .Concat(Enumerable.Repeat(source.Last().ToString(), 1));
+
+            var s = string.Concat(leadingMessage, openingBracket,
+                mutated.Aggregate((current, next) => string.Format($"{current}{next}")),
+                closingBracket,
+                (newLineAfterSequence ? Environment.NewLine : null));
+
+            s.Print(w, false);
+            return;
+        }
+
+        public static void Print(this string s, TextWriter writer, bool newLine = true)
+        {
+            if (s == null) throw new ArgumentNullException("s");
+
+            if (newLine) writer.WriteLine(s); else writer.Write(s);
+        }
+
+        public static bool IsOdd(this int n)
+        {
+            return (n & 1) == 1;
+        }
+
+        public static int Find<T>(this T[] array, T elementToFind)
+        {
+            if (array == null) throw new ArgumentNullException("array");
+
+            return Find<T>(array, elementToFind, 0, array.Length);
+        }
+
+        public static int Find<T>(this T[] array, T elementToFind, int startsSearchingAtIndex, int searchTillIndex)
+        {
+            var len = 0;
+
+            if (array == null) throw new ArgumentNullException("array");
+
+            if ((len = array.Length) == 0) return -1;
+
+            if (startsSearchingAtIndex < 0 
+                || startsSearchingAtIndex > searchTillIndex 
+                || searchTillIndex >= len)
+                throw new ArgumentOutOfRangeException("One or more of the search boundaries are invalid. The index to start the search must be less than or equal to the index to end searching. And the index to end the search must be less than the length of the source array.");
+
+            int start = startsSearchingAtIndex;
+            int end = searchTillIndex;
+
+            for (int i = start; i <= end; i++)
+                if (array[i].Equals(elementToFind))
+                    return i;
+
+            return -1;
+        }
+
+        public static void RemoveElementAt<T>(this T[] array, int index)
+        {
+            if (array == null) throw new ArgumentNullException();
+
+            var len = 0;
+            if ((len = array.Length) == 0) return;
+            if (index < 0 || index >= len) throw new ArgumentOutOfRangeException("index");
+
+            if (index == len - 1)
+            {
+                array[len - 1] = default(T);
+                return;
+            }
+
+            for(int i = index + 1; i < len; i++)
+            {
+                array[i - 1] = array[i];
+            }
+
+            array[len - 1] = default(T);
+        }
+    }
+}
---
+++ L2.Arrays.OddOccurrencesInArray/L2.Arrays.OddOccurrencesInArray/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace L2.Arrays.OddOccurrencesInArray
+{
+    // https://codility.com/programmers/task/odd_occurrences_in_array/
+    // Lesson 2: Arrays
+    // OddOccurrencesInArray
+    // Find value that occurs in odd number of elements.
+
+    // A non-empty zero-indexed array A consisting of N integers 
+    // is given. The array contains an odd number of elements, 
+    // and each element of the array can be paired with another 
+    // element that has the same value, except for one element 
+    // that is left unpaired.
+
+    // For example, in array A such that:
+    //   A[0] = 9  A[1] = 3  A[2] = 9
+    //   A[3] = 3  A[4] = 9  A[5] = 7
+    //   A[6] = 9
+
+    //         the elements at indexes 0 and 2 have value 9,
+    //         the elements at indexes 1 and 3 have value 3,
+    //         the elements at indexes 4 and 6 have value 9,
+    //         the element at index 5 has value 7 and is unpaired.
+
+    // Write a function:
+
+    //     int solution(int A[], int N);
+
+    // that, given an array A consisting of N integers fulfilling 
+    // the above conditions, returns the value of the unpaired element.
+
+    // For example, given array A such that:
+    //   A[0] = 9  A[1] = 3  A[2] = 9
+    //   A[3] = 3  A[4] = 9  A[5] = 7
+    //   A[6] = 9
+
+    // the function should return 7, as explained in the example above.
+
+
+    // I recall that this is the same as the HackerRank Lonely Integer problem:
+    // https://github.com/Sathyaish/HackerRank/tree/master/LonelyInteger
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = GetUserInput();
+            var len = 0;
+
+            if (array == null || (len = array.Length) == 0) return;
+
+            var unpaired = Solution(array, len);
+
+            array.Print(leadingMessage: "\nInput: ");
+            Console.WriteLine($"Unpaired integer: {unpaired}");
+            
+            Console.ReadKey();
+        }
+        
+        public static int Solution(int[] array, int len)
+        {
+            Debug.Assert(len.IsOdd());
+
+            var medianIndex = (len - 1) / 2;
+            var tempArray = new int[medianIndex];
+            tempArray[0] = array[medianIndex];
+            var tempArrayFilledTillIndex = 0;
+
+            int backward = medianIndex - 1;
+            int forward = medianIndex + 1;
+
+            while (backward >= 0 || forward < len)
+            {
+                if (backward >= 0)
+                {
+                    CompareElement(array, tempArray, ref backward, ref tempArrayFilledTillIndex);
+                    backward--;
+                }
+
+                if (forward < len)
+                {
+                    CompareElement(array, tempArray, ref forward, ref tempArrayFilledTillIndex);
+                    forward++;
+                }
+            }
+
+            return tempArray[0];
+        }
+
+        private static void CompareElement(int[] array, int[] tempArray, ref int index, ref int tempArrayFilledTillIndex)
+        {
+            var elementFoundAt = tempArray.Find<int>(array[index], 0, tempArrayFilledTillIndex);
+
+            if (elementFoundAt >= 0)
+            {
+                tempArray.RemoveElementAt(elementFoundAt);
+                tempArrayFilledTillIndex--;
+            }
+            else
+            {
+                tempArray[++tempArrayFilledTillIndex] = array[index];
+            }
+        }
+
+        private static int[] GetUserInput()
+        {
+            var s =
+                @"* Consider an array of integers where all but one of the integers 
+* occur in pairs. In other words, every element in occurs exactly 
+* twice except for one unique element.
+* Find and print the unique element.";
+
+            Console.WriteLine("The premise of this problem is as follows:\n");
+            Console.WriteLine(s);
+
+            Console.WriteLine("\nType in a bunch of numbers conforming to the array pattern described above, each number separated by a space:\n");
+
+            var list = new List<int>();
+
+            Console.ReadLine().Split()
+                .ToList()
+                .ForEach(t =>
+                {
+                    int n = 0;
+                    var b = int.TryParse(t, out n);
+                    if (b) list.Add(n);
+                });
+
+            if (list.Count == 0) return null;
+
+            return list.ToArray();
+        }
+    }
+}
