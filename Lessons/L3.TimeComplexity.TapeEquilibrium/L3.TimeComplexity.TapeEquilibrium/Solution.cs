0775319f7042b37efed10598e6c019ec43e82400
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2016-10-03 19:20:05 +0530

    // https://codility.com/programmers/task/tape_equilibrium/ // Lesson 3: Time Complexity // TapeEquilibrium // Minimize the value |(A[0] + ... + A[P-1]) - (A[P] + ... + A[N-1])|.
    /*
    
    A non-empty zero-indexed array A consisting of N integers is given. Array A represents numbers on a tape.
    
    Any integer P, such that 0 < P < N, splits this tape into two non-empty parts: A[0], A[1], ..., A[P − 1] and A[P], A[P + 1], ..., A[N − 1].
    
    The difference between the two parts is the value of: |(A[0] + A[1] + ... + A[P − 1]) − (A[P] + A[P + 1] + ... + A[N − 1])|
    
    In other words, it is the absolute difference between the sum of the first part and the sum of the second part.
    
    For example, consider array A such that:
      A[0] = 3
      A[1] = 1
      A[2] = 2
      A[3] = 4
      A[4] = 3
    
    We can split this tape in four places:
    
            P = 1, difference = |3 − 10| = 7
            P = 2, difference = |4 − 9| = 5
            P = 3, difference = |6 − 7| = 1
            P = 4, difference = |10 − 3| = 7
    
    Write a function:
    
        class Solution { public int solution(int[] A); }
    
    that, given a non-empty zero-indexed array A of N integers, returns the minimal difference that can be achieved.
    
    For example, given:
      A[0] = 3
      A[1] = 1
      A[2] = 2
      A[3] = 4
      A[4] = 3
    
    the function should return 1, as explained above.
    
    Assume that:
    
            N is an integer within the range [2..100,000];
            each element of array A is an integer within the range [−1,000..1,000].
    
    Complexity:
    
            expected worst-case time complexity is O(N);
            expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
    
    Elements of input arrays can be modified.
    
    */

---
+++ Lessons/L3.TimeComplexity.TapeEquilibrium/L3.TimeComplexity.TapeEquilibrium/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace L3.TimeComplexity.TapeEquilibrium
+{
+    public class Solution
+    {
+        // Linear time : O(n)
+        public int solution(int[] array)
+        {
+            if (array == null) throw new ArgumentNullException("array");
+
+            var len = array.Length;
+
+            if (len < 2) throw new ArgumentException("The array must have at least 2 elements.");
+
+            if (len == 2) return (array[0] - array[1]).AbsoluteValue();
+
+            // Make a one pass to get the sum
+            var sum = 0;
+            for (int i = 0; i < len; i++)
+                sum += array[i];
+
+            var sumL = array[0];
+            var sumR = sum - array[0];
+            var diff = (sumL - sumR).AbsoluteValue();
+            var minDiff = diff;
+
+            // Make a second pass to compute min diff
+            for(int i = 1; i < len - 1; i++)
+            {
+                sumL += array[i];
+                sumR -= array[i];
+                diff = (sumL - sumR).AbsoluteValue();
+                minDiff = Min(diff, minDiff);
+            }
+
+            return minDiff;
+        }
+
+        private int Min(int a, int b)
+        {
+            return (a < b) ? a : b;
+        }
+    }
+}
