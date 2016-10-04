cfc929b0e7042caa8da806ebdbd2493bb8d26b60
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2016-10-04 17:53:00 +0530

    Lesson 4: Counting Elements, FrogRiverOne

---
+++ Lessons/L4.CountingElements.FrogRiverOne/L4.CountingElements.FrogRiverOne/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace L4.CountingElements.FrogRiverOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new[] { 1, 3, 1, 4, 2, 3, 5, 4 }; // Enumerable.Range(1, 100).ToArray();
+
+            var s = new Solution();
+            var result = s.solution(5, array);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int x, int[] array)
+        {
+            var len = array.Length;
+            var shadow = new int[len + 1];
+            var sumTillX = (long)(x * (x + 1)) / 2;
+            var runningTotal = 0;
+
+            for (int i = 0; i < len; i++)
+            {
+                var current = array[i];
+
+                if (current < 0 || current > x) continue;
+
+                shadow[current] += 1;
+
+                if (shadow[current] == 1)
+                {
+                    runningTotal += current;
+
+                    if (runningTotal == sumTillX) return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
