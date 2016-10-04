d2e5461bb5750da3b3ff6565bf39c660f71fcefb
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2016-10-04 18:43:50 +0530

    Lesson 4: Counting Elements, MaxCounters

---
+++ Lessons/L4.CountingElements.MaxCounters/L4.CountingElements.MaxCounters/Extensions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace L4.CountingElements.MaxCounters
+{
+    public static class Extensions
+    {
+        public static void Print<T>(this IEnumerable<T> source,
+            TextWriter writer = null,
+            string separator = ", ",
+            string leadingMessage = null,
+            bool newLineAfterSequence = true,
+            bool throwOnEmptySource = true,
+            string emptySequenceMessage = "No items to print.",
+            string bracket = "[]")
+        {
+            TextWriter w = writer ?? Console.Out;
+            IEnumerable<string> mutated = null;
+            var count = 0;
+            var builder = new StringBuilder();
+            string openingBracket = null;
+            string closingBracket = null;
+
+            if (source == null || source.Count() == 0)
+            {
+                if (throwOnEmptySource) throw new ArgumentNullException("source");
+
+                emptySequenceMessage.Print(w, newLineAfterSequence);
+                return;
+            }
+
+            if (bracket != null && bracket.Length != 2)
+            {
+                throw new ArgumentException("Invalid bracket");
+            }
+
+            openingBracket = bracket[0].ToString();
+            closingBracket = bracket[1].ToString();
+
+            count = source.Count();
+            if (count == 1)
+            {
+                builder.AppendFormat($"{leadingMessage}{openingBracket}{source.First()?.ToString()}{closingBracket}");
+                builder.ToString().Print(w, newLineAfterSequence);
+                return;
+            }
+
+            mutated = source.Take(count - 1)
+                .Select(item => string.Format($"{item.ToString()}{separator}"))
+                .Concat(Enumerable.Repeat(source.Last().ToString(), 1));
+
+            var s = string.Concat(leadingMessage, openingBracket,
+                mutated.Aggregate((current, next) => string.Format($"{current}{next}")),
+                closingBracket,
+                (newLineAfterSequence ? Environment.NewLine : null));
+
+            s.Print(w, false);
+            return;
+        }
+
+        public static void Print(this string s, TextWriter writer, bool newLine = true)
+        {
+            if (s == null) throw new ArgumentNullException("s");
+
+            if (newLine) writer.WriteLine(s); else writer.Write(s);
+        }
+    }
+}
---
+++ Lessons/L4.CountingElements.MaxCounters/L4.CountingElements.MaxCounters/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace L4.CountingElements.MaxCounters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new[] { 3, 4, 4, 6, 1, 4, 4 };
+
+            var s = new Solution();
+            var result = s.solution(5, array);
+
+            result.Print(leadingMessage: "\nOutput array: ");
+
+            Console.ReadKey();
+        }
+    }
+
+    class Solution
+    {
+        public int[] solution(int n, int[] array)
+        {
+            var len = array.Length;
+            var counters = new int[n];
+            var max = 0;
+
+            for(int i = 0; i < len; i++)
+            {
+                var current = array[i];
+
+                if (current <= 0) throw new Exception("Invalid input. Array element cannot be less than 1.");
+
+                if (current <= n)
+                {
+                    increase(current - 1, counters);
+                    max = Max(max, counters[current - 1]);
+                }
+                else
+                {
+                    maxCounter(max, counters);
+                }
+            }
+
+            return counters;
+        }
+
+        private int Max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+
+        private void increase(int i, int[] array)
+        {
+            array[i] += 1;
+        }
+
+        private void maxCounter(int max, int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+                array[i] = max;
+        }
+    }
+}
