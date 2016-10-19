8492e029a4fa6748081645f967107f7abfe046ae
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2016-10-19 18:15:02 +0530

    I believe my solution is correct but the problem description and their test cases are wrong. Or at best, the description is ambiguous. I have written them an email about it asking for a clarification.
    /*
    
    https://codility.com/programmers/lessons/5-prefix_sums/min_avg_two_slice/
    
    MinAvgTwoSlice
    Find the minimal average of any slice containing at least two elements.
    
    A non-empty zero-indexed array A consisting of N integers is given. A pair of integers (P, Q), such that 0 ≤ P < Q < N, is called a slice of array A (notice that the slice contains at least two elements). The average of a slice (P, Q) is the sum of A[P] + A[P + 1] + ... + A[Q] divided by the length of the slice. To be precise, the average equals (A[P] + A[P + 1] + ... + A[Q]) / (Q − P + 1).
    
    For example, array A such that:
        A[0] = 4
        A[1] = 2
        A[2] = 2
        A[3] = 5
        A[4] = 1
        A[5] = 5
        A[6] = 8
    
    contains the following example slices:
    
            slice (1, 2), whose average is (2 + 2) / 2 = 2;
            slice (3, 4), whose average is (5 + 1) / 2 = 3;
            slice (1, 4), whose average is (2 + 2 + 5 + 1) / 4 = 2.5.
    
    The goal is to find the starting position of a slice whose average is minimal.
    
    Write a function:
    
        class Solution { public int solution(int[] A); }
    
    that, given a non-empty zero-indexed array A consisting of N integers, returns the starting position of the slice with the minimal average. If there is more than one slice with a minimal average, you should return the smallest starting position of such a slice.
    
    For example, given array A such that:
        A[0] = 4
        A[1] = 2
        A[2] = 2
        A[3] = 5
        A[4] = 1
        A[5] = 5
        A[6] = 8
    
    the function should return 1, as explained above.
    
    Assume that:
    
            N is an integer within the range [2..100,000];
            each element of array A is an integer within the range [−10,000..10,000].
    
    Complexity:
    
            expected worst-case time complexity is O(N);
            expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
    
    Elements of input arrays can be modified.
    
    */

---
+++ Lessons/L5.PrefixSums.MinAvgTwoSlice/L5.PrefixSums.MinAvgTwoSlice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace L5.PrefixSums.MinAvgTwoSlice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new[] { 4, 2, 2, 5, 1, 5, 8 };
+
+            var result = new Solution().solution(array);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Lessons/L5.PrefixSums.MinAvgTwoSlice/L5.PrefixSums.MinAvgTwoSlice/Solution.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace L5.PrefixSums.MinAvgTwoSlice
+{
+    public class Solution
+    {
+        public int solution(int[] array)
+        {
+            // Empty or null array
+            var nLen = array?.Length;
+
+            if (array == null || nLen == null || nLen == 0)
+                throw new ArgumentNullException("array");
+
+            var len = nLen.Value;
+
+            // Array with less than two elements
+            if (len < 2) throw new ArgumentException("array");
+
+            // Array with two elements
+            if (len == 2) return 0;
+
+            // Asymotes towards large values
+            double minAverage = double.MaxValue;
+            var prefixSums = GetPrefixSums(array);
+            var minIndex = -1;
+
+            for(int i = 0; i < len - 1; i++)
+            {
+                for (int j = i + 1; j < len; j++)
+                {
+                    int n = j - i + 1;
+                    long sumOfSlice = prefixSums[j + 1] - prefixSums[i];
+                    double average = sumOfSlice / n;
+
+                    if (average < minAverage)
+                    {
+                        minAverage = average;
+                        minIndex = i;
+                    }
+                }
+            }
+
+            return minIndex;
+        }
+
+        static long[] GetPrefixSums(int[] array)
+        {
+            var nLen = array?.Length;
+
+            if (array == null || nLen == null || nLen == 0)
+                throw new ArgumentNullException("array");
+
+            var len = nLen.Value;
+            var prefixSums = new long[len + 1];
+            prefixSums[0] = 0;
+            var runningTotal = 0;
+
+            for(int i = 1; i < len + 1; i++)
+            {
+                runningTotal += array[i - 1];
+                prefixSums[i] = runningTotal;    
+            }
+
+            return prefixSums;
+        }
+    }
+}
