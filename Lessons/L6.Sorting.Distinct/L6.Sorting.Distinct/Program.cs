87157404ce5b0e058173c30925ce0b888660f007
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2016-10-19 20:04:40 +0530

    /*
    https://codility.com/programmers/lessons/6-sorting/distinct/
    Distinct
    Compute number of distinct values in an array.
    
    Write a function
    
        class Solution { public int solution(int[] A); }
    
    that, given a zero-indexed array A consisting of N integers, returns the number of distinct values in array A.
    
    Assume that:
    
            N is an integer within the range [0..100,000];
            each element of array A is an integer within the range [−1,000,000..1,000,000].
    
    For example, given array A consisting of six elements such that:
     A[0] = 2    A[1] = 1    A[2] = 1
     A[3] = 2    A[4] = 3    A[5] = 1
    
    the function should return 3, because there are 3 distinct values appearing in array A, namely 1, 2 and 3.
    
    Complexity:
    
            expected worst-case time complexity is O(N*log(N));
            expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
    
    Elements of input arrays can be modified.
    
    */

---
+++ Lessons/L6.Sorting.Distinct/L6.Sorting.Distinct/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace L6.Sorting.Distinct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new[] { 2, 1, 1, 2, 3, 1 };
+            var n = new Solution().solution(array);
+
+            Console.WriteLine(n);
+        }
+    }
+}
---
+++ Lessons/L6.Sorting.Distinct/L6.Sorting.Distinct/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace L6.Sorting.Distinct
+{
+    public class Solution
+    {
+        public int solution(int[] array)
+        {
+            if (array == null) throw new ArgumentNullException("array");
+
+            var len = array.Length;
+            if (len <= 1) return len;
+
+            Array.Sort<int>(array);
+            var counter = 1;
+
+            for(int i = 0; i < len - 1; i++)
+                if (array[i] != array[i + 1])
+                    counter++;
+
+            return counter;
+        }
+
+        public int UsingLINQ(int[] array)
+        {
+            Array.Sort(array);
+
+            return array.Distinct().Count();
+        }
+    }
+}
