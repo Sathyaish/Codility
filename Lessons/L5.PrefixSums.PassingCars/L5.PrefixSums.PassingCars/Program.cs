7bb31c384b2bcda50913f18f79231e8dd39b78ee
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2016-10-19 14:57:39 +0530

    /*
    https://codility.com/programmers/lessons/5-prefix_sums/passing_cars/
    PassingCars
    Count the number of passing cars on the road.
    
    A non-empty zero-indexed array A consisting of N integers is given. The consecutive elements of array A represent consecutive cars on a road.
    
    Array A contains only 0s and/or 1s:
    
            0 represents a car traveling east,
            1 represents a car traveling west.
    
    The goal is to count passing cars. We say that a pair of cars (P, Q), where 0 ≤ P < Q < N, is passing when P is traveling to the east and Q is traveling to the west.
    
    For example, consider array A such that:
      A[0] = 0
      A[1] = 1
      A[2] = 0
      A[3] = 1
      A[4] = 1
    
    We have five pairs of passing cars: (0, 1), (0, 3), (0, 4), (2, 3), (2, 4).
    
    Write a function:
    
        class Solution { public int solution(int[] A); }
    
    that, given a non-empty zero-indexed array A of N integers, returns the number of pairs of passing cars.
    
    The function should return −1 if the number of pairs of passing cars exceeds 1,000,000,000.
    
    For example, given:
      A[0] = 0
      A[1] = 1
      A[2] = 0
      A[3] = 1
      A[4] = 1
    
    the function should return 5, as explained above.
    
    Assume that:
    
            N is an integer within the range [1..100,000];
            each element of array A is an integer that can have one of the following values: 0, 1.
    
    Complexity:
    
            expected worst-case time complexity is O(N);
            expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).
    
    Elements of input arrays can be modified.
    
    */

---
+++ Lessons/L5.PrefixSums.PassingCars/L5.PrefixSums.PassingCars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace L5.PrefixSums.PassingCars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new[] { 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0 };
+
+            var maxPairs = new Solution().solution(array);
+
+            Console.WriteLine($"Max pairs: {maxPairs}");
+        }
+    }
+}
---
+++ Lessons/L5.PrefixSums.PassingCars/L5.PrefixSums.PassingCars/Solution.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace L5.PrefixSums.PassingCars
+{
+    public class Solution
+    {
+        public int solution(int[] array)
+        {
+            var nLen = array?.Length;
+
+            if (array == null || nLen == null || nLen == 0)
+                throw new ArgumentNullException("array");
+
+            if (nLen < 2)
+                throw new ArgumentException("Input array must have at least 2 elements.");
+
+            var len = nLen.Value;
+
+            var numPairs = 0;
+            var prefixSums = GetPrefixSums(array);
+
+            for (int i = 0; i < len; i++)
+            {
+                if (array[i] != 0) continue;
+
+                numPairs += GetSumOfSlice(prefixSums, i, len - 1);
+            }
+
+            return numPairs > 1000000000 ? -1 : numPairs;
+        }
+        
+        private int GetSumOfSlice(int[] prefixSums, int from, int to)
+        {
+            return prefixSums[to + 1] - prefixSums[from];
+        }
+
+        private int[] GetPrefixSums(int[] array)
+        {
+            var nLen = array?.Length;
+
+            if (array == null || nLen == null || nLen == 0)
+                throw new ArgumentNullException("array");
+
+            var len = nLen.Value;
+
+            if (len == 1) return new[] { 0, array[0] };
+
+            var prefixSums = new int[len + 1];
+            prefixSums[0] = array[0];
+            int runningTotal = 0;
+
+            for(int i = 1; i < len + 1; i++)
+            {
+                runningTotal += array[i - 1];
+                prefixSums[i] = runningTotal;
+            }
+
+            return prefixSums;
+        }
+    }
+}
