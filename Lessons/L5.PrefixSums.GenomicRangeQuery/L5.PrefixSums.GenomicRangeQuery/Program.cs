8c43900cac00848bf0503c7063e5ffc47051383f
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2016-10-19 17:03:47 +0530

    /*
    https://codility.com/programmers/lessons/5-prefix_sums/genomic_range_query/
    GenomicRangeQuery
    Find the minimal nucleotide from a range of sequence DNA.
    
    A DNA sequence can be represented as a string consisting of the letters A, C, G and T, which correspond to the types of successive nucleotides in the sequence. Each nucleotide has an impact factor, which is an integer. Nucleotides of types A, C, G and T have impact factors of 1, 2, 3 and 4, respectively. You are going to answer several queries of the form: What is the minimal impact factor of nucleotides contained in a particular part of the given DNA sequence?
    
    The DNA sequence is given as a non-empty string S = S[0]S[1]...S[N-1] consisting of N characters. There are M queries, which are given in non-empty arrays P and Q, each consisting of M integers. The K-th query (0 ≤ K < M) requires you to find the minimal impact factor of nucleotides contained in the DNA sequence between positions P[K] and Q[K] (inclusive).
    
    For example, consider string S = CAGCCTA and arrays P, Q such that:
        P[0] = 2    Q[0] = 4
        P[1] = 5    Q[1] = 5
        P[2] = 0    Q[2] = 6
    
    The answers to these M = 3 queries are as follows:
    
            The part of the DNA between positions 2 and 4 contains nucleotides G and C (twice), whose impact factors are 3 and 2 respectively, so the answer is 2.
            The part between positions 5 and 5 contains a single nucleotide T, whose impact factor is 4, so the answer is 4.
            The part between positions 0 and 6 (the whole string) contains all nucleotides, in particular nucleotide A whose impact factor is 1, so the answer is 1.
    
    Write a function:
    
        class Solution { public int[] solution(string S, int[] P, int[] Q); }
    
    that, given a non-empty zero-indexed string S consisting of N characters and two non-empty zero-indexed arrays P and Q consisting of M integers, returns an array consisting of M integers specifying the consecutive answers to all queries.
    
    The sequence should be returned as:
    
            a Results structure (in C), or
            a vector of integers (in C++), or
            a Results record (in Pascal), or
            an array of integers (in any other programming language).
    
    For example, given the string S = CAGCCTA and arrays P, Q such that:
        P[0] = 2    Q[0] = 4
        P[1] = 5    Q[1] = 5
        P[2] = 0    Q[2] = 6
    
    the function should return the values [2, 4, 1], as explained above.
    
    Assume that:
    
            N is an integer within the range [1..100,000];
            M is an integer within the range [1..50,000];
            each element of arrays P, Q is an integer within the range [0..N − 1];
            P[K] ≤ Q[K], where 0 ≤ K < M;
            string S consists only of upper-case English letters A, C, G, T.
    
    Complexity:
    
            expected worst-case time complexity is O(N+M);
            expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
    
    Elements of input arrays can be modified.
    
    */

---
+++ Lessons/L5.PrefixSums.GenomicRangeQuery/L5.PrefixSums.GenomicRangeQuery/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace L5.PrefixSums.GenomicRangeQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "CAGCCTA";
+            var p = new [] { 2, 5, 0 };
+            var q = new[] { 4, 5, 6 };
+
+            var result = new Solution().solution(s, p, q);
+
+            PrintArray(result);
+        }
+
+        private static void PrintArray(int[] result)
+        {
+            for(int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine($"{result[i]}");
+            }
+        }
+    }
+}
---
+++ Lessons/L5.PrefixSums.GenomicRangeQuery/L5.PrefixSums.GenomicRangeQuery/Solution.cs
@@ -0,0 +1,79 @@
+﻿namespace L5.PrefixSums.GenomicRangeQuery
+{
+    public class Solution
+    {
+        public int[] solution(string s, int[] p, int[] q)
+        {
+            var prefixSums = GetPrefixSums(s);
+
+            var len = p.Length;
+            var result = new int[len];
+
+            for(int m = 0; m < len; m++)
+            {
+                var from = p[m];
+                var to = q[m];
+
+                if (prefixSums.PA[to + 1] - prefixSums.PA[from] > 0)
+                {
+                    result[m] = 1;
+                }
+                else if (prefixSums.PC[to + 1] - prefixSums.PC[from] > 0)
+                {
+                    result[m] = 2;
+                }
+                else if (prefixSums.PG[to + 1] - prefixSums.PG[from] > 0)
+                {
+                    result[m] = 3;
+                }
+                else
+                {
+                    result[m] = 4;
+                }
+            }
+
+            return result;
+        }
+
+        private PrefixSums GetPrefixSums(string s)
+        {
+            var len = s.Length;
+
+            var a = new int[len + 1];
+            var c = new int[len + 1];
+            var g = new int[len + 1];
+
+            a[0] = c[0] = g[0] = 0;
+            int nA = 0, nC = 0, nG = 0;
+
+            for(int i = 1; i < len + 1; i++)
+            {
+                if (s[i - 1] == 'A')
+                {
+                    nA++;
+                }
+                else if (s[i - 1] == 'C')
+                {
+                    nC++;
+                }
+                else if (s[i - 1] == 'G')
+                {
+                    nG++;
+                }
+
+                a[i] = nA;
+                c[i] = nC;
+                g[i] = nG;
+            }
+
+            return new PrefixSums { PA = a, PC = c, PG = g };
+        }
+    }
+
+    public class PrefixSums
+    {
+        public int[] PA { get; set; }
+        public int[] PC { get; set; }
+        public int[] PG { get; set; }
+    }
+}
