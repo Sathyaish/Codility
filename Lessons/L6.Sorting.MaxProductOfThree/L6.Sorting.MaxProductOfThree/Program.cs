7cf2076628dc475d476170c2a046f9e2f90a3a47
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2016-10-20 07:48:14 +0530

    /*
    https://codility.com/programmers/lessons/6-sorting/max_product_of_three/
    
    MaxProductOfThree
    Maximize A[P] * A[Q] * A[R] for any triplet (P, Q, R).
    
    A non-empty zero-indexed array A consisting of N integers is given. The product of triplet (P, Q, R) equates to A[P] * A[Q] * A[R] (0 ≤ P < Q < R < N).
    
    For example, array A such that:
      A[0] = -3
      A[1] = 1
      A[2] = 2
      A[3] = -2
      A[4] = 5
      A[5] = 6
    
    contains the following example triplets:
    
            (0, 1, 2), product is −3 * 1 * 2 = −6
            (1, 2, 4), product is 1 * 2 * 5 = 10
            (2, 4, 5), product is 2 * 5 * 6 = 60
    
    Your goal is to find the maximal product of any triplet.
    
    Write a function:
    
        class Solution { public int solution(int[] A); }
    
    that, given a non-empty zero-indexed array A, returns the value of the maximal product of any triplet.
    
    For example, given array A such that:
      A[0] = -3
      A[1] = 1
      A[2] = 2
      A[3] = -2
      A[4] = 5
      A[5] = 6
    
    the function should return 60, as the product of triplet (2, 4, 5) is maximal.
    
    Assume that:
    
            N is an integer within the range [3..100,000];
            each element of array A is an integer within the range [−1,000..1,000].
    
    Complexity:
    
            expected worst-case time complexity is O(N*log(N));
            expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).
    
    Elements of input arrays can be modified.
    
    */

---
+++ Lessons/L6.Sorting.MaxProductOfThree/L6.Sorting.MaxProductOfThree/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace L6.Sorting.MaxProductOfThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new[] { -3, 1, 2, -2, 5, 6 };
+
+            var maxProduct = new Solution().solution(array);
+
+            Console.WriteLine($"Maximum product: {maxProduct}");
+        }
+    }
+}
---
+++ Lessons/L6.Sorting.MaxProductOfThree/L6.Sorting.MaxProductOfThree/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace L6.Sorting.MaxProductOfThree
+{
+    public class Solution
+    {
+        public int solution(int[] array)
+        {
+            var nLen = array?.Length;
+
+            if (array == null || nLen == null)
+            {
+                throw new ArgumentNullException("array");
+            }
+
+            var len = nLen.Value;
+
+            if (len < 3)
+            {
+                throw new ArgumentException("The input array must have at least 3 elements.");
+            }
+
+            if (len == 3) return array[0] * array[1] * array[2];
+            
+            Array.Sort(array);
+
+            var productOfLastThreeTerms = array[len - 1] * array[len - 2] * array[len - 3];
+
+            var product = int.MinValue;
+            if (array[0] < 0 && array[1] < 0 && array[len - 1] > 0)
+            {
+                product = array[0] * array[1] * array[len - 1];
+            }
+
+            return productOfLastThreeTerms > product ? productOfLastThreeTerms : product;
+        }
+    }
+}
