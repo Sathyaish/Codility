2d5d76de9152238c4e343bff6d85c2ad2d2b754c
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2016-10-20 09:55:10 +0530

    /*
    https://codility.com/programmers/lessons/6-sorting/triangle/
    Triangle
    Determine whether a triangle can be built from a given set of edges.
    
    A zero-indexed array A consisting of N integers is given. A triplet (P, Q, R) is triangular if 0 ≤ P < Q < R < N and:
    
            A[P] + A[Q] > A[R],
            A[Q] + A[R] > A[P],
            A[R] + A[P] > A[Q].
    
    For example, consider array A such that:
      A[0] = 10    A[1] = 2    A[2] = 5
      A[3] = 1     A[4] = 8    A[5] = 20
    
    Triplet (0, 2, 4) is triangular.
    
    Write a function:
    
        class Solution { public int solution(int[] A); }
    
    that, given a zero-indexed array A consisting of N integers, returns 1 if there exists a triangular triplet for this array and returns 0 otherwise.
    
    For example, given array A such that:
      A[0] = 10    A[1] = 2    A[2] = 5
      A[3] = 1     A[4] = 8    A[5] = 20
    
    the function should return 1, as explained above. Given array A such that:
      A[0] = 10    A[1] = 50    A[2] = 5
      A[3] = 1
    
    the function should return 0.
    
    Assume that:
    
            N is an integer within the range [0..100,000];
            each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].
    
    Complexity:
    
            expected worst-case time complexity is O(N*log(N));
            expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
    
    Elements of input arrays can be modified.
    
    */

---
+++ Lessons/L6.Sorting.Triangle/L6.Sorting.Triangle/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace L6.Sorting.Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new[] { 10, 2, 5, 1, 8, 20 };
+
+            var result = new Solution().solution(array);
+
+            Console.WriteLine($"{(result != 0).ToString()}");
+        }
+    }
+}
---
+++ Lessons/L6.Sorting.Triangle/L6.Sorting.Triangle/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace L6.Sorting.Triangle
+{
+    public class Solution
+    {
+        public int solution(int[] array)
+        {
+            var nLen = array?.Length;
+
+            if (array == null || nLen == null)
+            {
+                throw new ArgumentNullException("array");
+            }
+
+            var len = nLen.Value;
+
+            if (len < 3) return 0;
+
+            Array.Sort(array);
+
+            var last = array[len - 1];
+            if (last < 0) return 0;
+            
+            for (int i = 0; i < len - 2; i++)
+            {
+                if (array[i] + array[i + 1] > array[i + 2]) return 1;
+            }
+
+            return 0;
+        }
+    }
+}
