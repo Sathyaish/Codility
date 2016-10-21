eb20a54ecec885b7df3f11e65e5d3094e4c23f11
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2016-10-21 11:38:50 +0530

    https://codility.com/programmers/lessons/7-stacks_and_queues/brackets/
    Brackets
    Determine whether a given string of parentheses is properly nested.
    
    A string S consisting of N characters is considered to be properly nested if any of the following conditions is true:
    
            S is empty;
            S has the form "(U)" or "[U]" or "{U}" where U is a properly nested string;
            S has the form "VW" where V and W are properly nested strings.
    
    For example, the string "{[()()]}" is properly nested but "([)()]" is not.
    
    Write a function:
    
        int solution(char *S);
    
    that, given a string S consisting of N characters, returns 1 if S is properly nested and 0 otherwise.
    
    For example, given S = "{[()()]}", the function should return 1 and given S = "([)()]", the function should return 0, as explained above.
    
    Assume that:
    
            N is an integer within the range [0..200,000];
            string S consists only of the following characters: "(", "{", "[", "]", "}" and/or ")".
    
    Complexity:
    
            expected worst-case time complexity is O(N);
            expected worst-case space complexity is O(N) (not counting the storage required for input arguments).

---
+++ Lessons/L7.StacksAndQueues.Brackets/L7.StacksAndQueues.Brackets/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace L7.StacksAndQueues.Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string s = null; // exception expected
+            // var s = string.Empty; // proper
+            // var s = "{"; // improper
+            // var s = "{[()()]}"; // proper
+            // var s = "{}{}{}{}[][][][][{()}]"; // proper
+            // var s = "{}{}{}{}[][][][][{()]}"; // improper
+            // var s = "([)()]"; // improper
+
+            try
+            {
+                var result = new Solution().solution(s);
+
+                Console.WriteLine((result == 1).ToString());
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Lessons/L7.StacksAndQueues.Brackets/L7.StacksAndQueues.Brackets/Solution.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace L7.StacksAndQueues.Brackets
+{
+    public class Solution
+    {
+        private const int Yes = 1;
+        private const int No = 0;
+        
+        // I would ideally store the bracket types
+        // in a dictionary with their key being the closing
+        // one and the value being the opening one or vice-versa
+        // but they want a space complexity solution of O(N)
+        // so I'll just resort to inline if else statements.
+        // Bracket types: (), {}, []
+        public int solution(string s)
+        {
+            if (s == null) throw new ArgumentNullException("s");
+
+            s = s.Trim();
+            var len = s.Length;
+
+            if (len == 1) return No;
+
+            var stack = new char[len];
+            var curr = 0;
+
+            for (int i = 0; i < len; i++)
+            {
+                if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+                {
+                    stack[curr++] = s[i];
+                }
+                else
+                {
+                    curr--;
+
+                    if (curr < 0) return No;
+
+                    if (s[i] == ')')
+                    {
+                        if (stack[curr] != '(') return No;
+                    }
+                    else if (s[i] == '}')
+                    {
+                        if (stack[curr] != '{') return No;
+                    }
+                    else if (s[i] == ']')
+                    {
+                        if (stack[curr] != '[') return No;
+                    }
+                }
+            }
+
+            return curr == 0 ? Yes : No;
+        }
+    }
+}
