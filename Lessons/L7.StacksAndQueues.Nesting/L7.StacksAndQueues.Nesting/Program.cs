269c98a9feaf59a12909719fa8d10b246ccc65bd
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2016-10-21 11:12:57 +0530

    https://codility.com/programmers/lessons/7-stacks_and_queues/nesting/
    Lesson 7
    Nesting
    Determine whether given string of parentheses is properly nested.
    
    A string S consisting of N characters is called properly nested if:
    
            S is empty;
            S has the form "(U)" where U is a properly nested string;
            S has the form "VW" where V and W are properly nested strings.
    
    For example, string "(()(())())" is properly nested but string "())" isn't.
    
    Write a function:
    
        class Solution { public int solution(string S); }
    
    that, given a string S consisting of N characters, returns 1 if string S is properly nested and 0 otherwise.
    
    For example, given S = "(()(())())", the function should return 1 and given S = "())", the function should return 0, as explained above.
    
    Assume that:
    
            N is an integer within the range [0..1,000,000];
            string S consists only of the characters "(" and/or ")".
    
    Complexity:
    
            expected worst-case time complexity is O(N);
            expected worst-case space complexity is O(1) (not counting the storage required for input arguments).

---
+++ Lessons/L7.StacksAndQueues.Nesting/L7.StacksAndQueues.Nesting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace L7.StacksAndQueues.Nesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var s = "(()(())())";
+            // string s = null;
+            // string s = string.Empty;
+            // var s = "())";
+            // var s = "(";
+            var s = "))))((((";
+
+            var result = new Solution().solution2(s);
+
+            Console.WriteLine((result == 1).ToString());
+        }
+    }
+}
---
+++ Lessons/L7.StacksAndQueues.Nesting/L7.StacksAndQueues.Nesting/Solution.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace L7.StacksAndQueues.Nesting
+{
+    public class Solution
+    {
+        private const int Yes = 1;
+        private const int No = 0;
+
+        public int solution(string s)
+        {
+            // Null string
+            if (s == null) throw new ArgumentNullException("s");
+
+            // Empty string
+            s = s.Trim();
+            var len = s.Length;
+            if (len == 0) return Yes;
+            if (len == 1) return No;
+
+            var stack = new int[len];
+            var curr = 0;
+
+            for(int i = 0; i < len; i++)
+            {
+                if (curr < 0) return No;
+
+                if (s[i] == '(')
+                    stack[curr++] = s[i];
+                else
+                    curr--;
+            }
+
+            return curr == 0 ? Yes : No;
+        }
+
+        public int solution2(string s)
+        {
+            // Null string
+            if (s == null) throw new ArgumentNullException("s");
+
+            // Empty string
+            s = s.Trim();
+            var len = s.Length;
+            if (len == 0) return Yes;
+            if (len == 1) return No;
+
+            var pendingOpenParens = 0;
+
+            for (int i = 0; i < len; i++)
+            {
+                if (pendingOpenParens < 0) return No;
+
+                if (s[i] == '(')
+                    pendingOpenParens++;
+                else
+                    pendingOpenParens--;
+            }
+
+            return pendingOpenParens == 0 ? Yes : No;
+        }
+    }
+}
