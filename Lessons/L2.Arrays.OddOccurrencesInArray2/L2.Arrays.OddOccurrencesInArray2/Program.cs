63307131a849c569c654da8a3599bc59dbad3dd6
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2016-10-22 09:48:39 +0530

    // My previous solution to this (also checked in into this same     // git repository) was *so* inefficient. I realize this now         // having done 25 odd problems or so. 	    // Here's another stab at a more efficient solution to this 	    // problem. 	        // https://codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/

---
+++ Lessons/L2.Arrays.OddOccurrencesInArray2/L2.Arrays.OddOccurrencesInArray2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace L2.Arrays.OddOccurrencesInArray2
+{
+    // My previous solution to this (also checked in into this same
+    // git repository) was *so* inefficient. I realize this now
+    // having done 25 odd problems or so.
+    // Here's another stab at a more efficient solution to this problem.
+    // https://codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new[] { 9, 3, 9, 3, 9, 7, 9 };
+
+            var oddOneOut = new Solution().solution(array);
+
+            Console.WriteLine(oddOneOut);
+        }
+    }
+}
---
+++ Lessons/L2.Arrays.OddOccurrencesInArray2/L2.Arrays.OddOccurrencesInArray2/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Solution
+{
+    internal int solution(int[] array)
+    {
+        if (array == null) throw new ArgumentNullException("array");
+
+        var len = array.Length;
+
+        if (len == 0) throw new ArgumentException();
+
+        if (len == 1) return array[0];
+
+        Array.Sort(array);
+
+        for (int i = 0; i < len - 1; i += 2)
+            if (array[i] != array[i + 1]) return array[i];
+
+        return array[len - 1];
+    }
+}
