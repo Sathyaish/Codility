e3d4e684c4fdeb4abdbde2ef20da53f54c46beed
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2016-10-24 18:53:27 +0530

    L10.Prime.MinPermiterRectangle.

---
+++ Lessons/L10.Prime.MinPerimeterRectangle/L10.Prime.MinPerimeterRectangle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Test(1);
+        Test(100);
+        Test(30);
+    }
+
+    public static void Test(int n)
+    {
+        try
+        {
+            int result = new Solution().solution(n);
+            Console.WriteLine(result);
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        Console.WriteLine();
+    }
+}
---
+++ Lessons/L10.Prime.MinPerimeterRectangle/L10.Prime.MinPerimeterRectangle/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+internal class Solution
+{
+    internal int solution(int n)
+    {
+        if (n == 1) return 4;
+
+        long perimeter = 0L;
+        long min = 0L;
+
+        var sqrt = (int)Math.Sqrt(n);
+
+        for(int i = 1; i <= sqrt; i++)
+        {
+            if (n % i == 0)
+            {
+                var factor1 = n / i;
+                var factor2 = n / factor1;
+
+                perimeter = 2 * (factor1 + factor2);
+
+                if (perimeter <= int.MaxValue)
+                {
+                    if (i == 1)
+                        min = perimeter;
+                    else
+                        min = min < perimeter ? min : perimeter;
+                }
+            }
+        }
+
+        return (int) min;
+    }
+}
