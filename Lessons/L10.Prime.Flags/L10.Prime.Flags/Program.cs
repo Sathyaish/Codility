c1ef999da35bb9d169c1a27b88906d207519fe96
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2016-10-25 11:03:22 +0530

    Lesson 10, Prime and Composite Numbers, Flags

---
+++ Lessons/L10.Prime.Flags/L10.Prime.Flags/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // The example they have provided
+        Test(new[] { 1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2 });
+
+        // Empty
+        Test(new int[] { });
+
+        // Single element
+        Test(new[] { 1 });
+
+        // Two element
+        Test(new[] { 1, 1 });
+        Test(new[] { 1, 2 });
+        Test(new[] { 2, 1 });
+
+        // Three element simple case with a peak
+        Test(new[] { 1, 3, 2 });
+
+        // Three element simple case with no peaks
+        Test(new[] { 1, 2, 3 });
+
+        // 100 element with no peaks
+        Test(Enumerable.Range(100, 100).ToArray());
+
+        // 20 element with maximum number of peaks
+        Test(new[] { 4, 7, 6, 11, 2, 9, 4, 12, 1, 100, 92, 93, 0, 5, 4, 8, 2, 10, 9, 11 });
+
+        // A series from the lowest possible to the largest possible element
+        Test(Enumerable.Range(1, 400000 - 1).ToArray());
+
+        // Max number of elements of largest possible values with no peaks
+        
+        // Max number of elements of largest possible values and maximum peaks
+
+        // Max number of elements of largest possible values with only 1 peak
+    }
+
+    public static void Test(int[] array)
+    {
+        try
+        {
+            int result = new Solution().solution(array);
+            Console.WriteLine(result);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        Console.WriteLine();
+    }
+}
---
+++ Lessons/L10.Prime.Flags/L10.Prime.Flags/Solution.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+internal class Solution
+{
+    internal int solution(int[] array)
+    {
+        // null input
+        if (array == null)
+            throw new ArgumentNullException("array");
+
+        var len = array.Length;
+
+        // single or two element array
+        if (len <= 2) return 0;
+
+        // Find all peaks
+        var maxPossiblePeaks = (int)(len - 1) / 2;
+        var peaks = new int[maxPossiblePeaks];
+        var k = 0; // number of peaks
+        for(int i = 1; i < len - 1; i++)
+        {
+            if ((array[i - 1] < array[i]) && (array[i] > array[i + 1]))
+            {
+                peaks[k++] = i;
+            }
+        }
+
+        if (k < 2) return k;
+
+        if (k == 2) return ((AbsoluteValue(peaks[0] - peaks[1]) >= k) ? 1 : 0);
+
+        // Filter only those peaks that satisfy the absolute value
+        // condition
+        var maxPossibleFlags = 0;
+        var from = 0;
+        var to = from + 1;
+        while (to < k)
+        {
+            if (AbsoluteValue(peaks[from] - peaks[to]) >= k)
+            {
+                if (maxPossibleFlags == 0) maxPossibleFlags = 1;
+
+                maxPossibleFlags++;
+
+                from = to;
+                to++;
+            }
+            else
+            {
+                to++;
+            }
+        }
+
+        return maxPossibleFlags;
+    }
+
+    static int AbsoluteValue(int n)
+    {
+        return n >= 0 ? n : -1 * n;
+    }
+}
