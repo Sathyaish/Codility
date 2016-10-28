307c2a4d95e2bafda346f88b18c6cb783c85654f
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2016-10-28 14:44:25 +0530

    Lesson 16, Greedy Algorithms, Reading Material, Exercise 1, Work in progress.

---
+++ Lessons/L16.Greedy.RM.Exercise1/L16.Greedy.RM.Exercise1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+    For a given set of denominations, you are asked to find the minimum number of coins with
+    which a given amount of money can be paid. That problem can be approached by a greedy
+    algorithm that always selects the largest denomination not exceeding the remaining amount
+    of money to be paid. As long as the remaining amount is greater than zero, the process is
+    repeated.
+     */
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine();
+    }
+}
---
+++ Lessons/L16.Greedy.RM.Exercise1/L16.Greedy.RM.Exercise1/Solution.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+
+    // To be continued tomorrow
+
+    public Set[] solution(int[] denominations, int amount)
+    {
+        if (denominations == null)
+            throw new ArgumentNullException("denominations");
+
+        var len = denominations.Length;
+
+        if (len == 1)
+        {
+            var d = denominations[0];
+
+            if (d > amount) return new Set[] { new Set(d, 1) };
+
+            var q = amount % d;
+            var dividend = amount / d;
+            var n = (q == 0) ? dividend : ++dividend;
+
+            return new Set[] { new Set(d, n) };
+        }
+
+        Array.Sort(denominations);
+
+        var sets = new List<Set>();
+        var remainingAmount = amount;
+
+        for(int i = len - 1; ((i >= 0) || (remainingAmount == 0)); i--)
+        {
+            var d = denominations[i];
+            var q = remainingAmount % d;
+            var dividend = remainingAmount / d;
+
+            
+        }
+
+        return sets?.ToArray();
+    }
+}
+
+public class Set
+{
+    public Set(int denomination, int numberOfCoins)
+    {
+        Denomination = denomination;
+        NumberOfCoins = numberOfCoins;
+    }
+
+    public int Denomination { get; set; }
+    public int NumberOfCoins { get; set; }
+}
